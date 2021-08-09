0f0279a46ea1d1b9a89adda644b6f885a0a4c8ea
jpam824 <jackpamukci824@gmail.com>
2021-08-09 19:58:34 -0400

    something small

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -108,7 +108,7 @@ public class Model extends Observable {
      * */
     public boolean tilt(Side side) {
         boolean changed;
-        changed = false;
+        changed = true;
 
         // TODO: Modify this.board (and perhaps this.score) to account
         // for the tilt to the Side SIDE. If the board changed, set the
